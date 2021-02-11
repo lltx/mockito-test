fe0cd7d8f1dec0b1ffa457e5a991699f94a47567
lengleng <wangiegie@163.com>
2021-02-11 12:10:02 +0800

    :memo: Writing docs. StubbingTests.java

--- src/test/java/com/pig4cloud/test/demotest/lesson4/StubbingTests.java
+++ src/test/java/com/pig4cloud/test/demotest/lesson4/StubbingTests.java
@@ -2,15 +2,19 @@ package com.pig4cloud.test.demotest.lesson4;
 
 import java.util.List;
 
+import com.pig4cloud.test.demotest.service.DemoServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
@@ -46,9 +50,52 @@ public class StubbingTests {
 
 	@Test
 	@DisplayName("stub 执行报错")
-	public void test() {
+	public void testDoThrow() {
 		doThrow(RuntimeException.class).when(list).clear();
-		assertThrows(RuntimeException.class,()-> list.clear());
+		assertThrows(RuntimeException.class, () -> list.clear());
+	}
+
+	@Test
+	@DisplayName("迭代器的模拟,每次获取的值都不一样")
+	public void testDoIterator() {
+		when(list.size()).thenReturn(1, 2, 3, 4);
+		assertEquals(1, list.size());
+		assertEquals(2, list.size());
+		assertEquals(3, list.size());
+		assertEquals(4, list.size());
+	}
+
+	@Test
+	@DisplayName("迭代器的模拟，anyInt")
+	public void testDoIteratorAnyInt() {
+		when(list.get(anyInt())).thenReturn("a", "b", "c", "d");
+		assertEquals("a", list.get(1));
+		assertEquals("b", list.get(2));
+		assertEquals("c", list.get(3));
+		assertEquals("d", list.get(4));
+	}
+
+	@Test
+	@DisplayName("迭代器的业务逻辑模拟 使用 thenAnswer invocation")
+	public void testDoAnswer() {
+		when(list.get(anyInt())).thenAnswer(invocation -> {
+			Integer argument = invocation.getArgument(0, Integer.class);
+			return argument.toString();
+		});
+
+		assertEquals("1", list.get(1));
+		assertEquals("2", list.get(2));
+		assertEquals("3", list.get(3));
+		assertEquals("4", list.get(4));
+	}
+
+	@Test
+	@DisplayName("call real method")
+	public void testCallRealMethod() {
+		DemoServiceImpl service = Mockito.mock(DemoServiceImpl.class);
+		when(service.demo()).thenCallRealMethod();
+		System.out.println(service.demo());
+		System.out.println(service.demo1());
 	}
 
 }
